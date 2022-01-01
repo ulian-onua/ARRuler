a7898f575b26fd90cbd830d4cfd7343317784d67
Julian Drapailo <ulian.onua@gmail.com>
2022-01-01 23:43:58 +0200

    Remove storyboard and add view controller to Scene programmatically

--- ARRuler/ViewController.swift
+++ ARRuler/ViewController.swift
@@ -4,6 +4,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = UIColor.yellow
         // Do any additional setup after loading the view.
     }
 
